9e8d48b7b28ec8cb6922ed6a3fd60862afd612fc
RamsyFocho <fochomukomr@gmail.com>
2025-06-07 18:56:57 +0100

    added the height of a container

--- components/home/featured-drinks.tsx
+++ components/home/featured-drinks.tsx
@@ -99,7 +99,7 @@ const FeatureDrinks = () => {
 
         {/* Product Grid - Single row with dynamic tab content */}
         <div
-          className="grid grid-cols-2 lg:grid-cols-4 gap-4 relative overflow-hidden min-h-[280px] md:min-h-[350px]"
+          className="grid grid-cols-2 lg:grid-cols-4 gap-4 relative overflow-hidden min-h-[290px] md:min-h-[350px]"
           // style={{ minHeight: 420 }}
         >
           <div
