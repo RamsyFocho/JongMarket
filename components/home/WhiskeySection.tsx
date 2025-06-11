fdbcbe08c9dfd9a75caf2f8e50d8aeb78f069ec6
RamsyFocho <fochomukomr@gmail.com>
2025-06-11 13:34:17 +0100

    changed some height

--- components/home/WhiskeySection.tsx
+++ components/home/WhiskeySection.tsx
@@ -107,7 +107,7 @@ const WhiskeySection = () => {
 
         {/* Product Grid - Single row with dynamic tab content */}
         <div
-          className="grid grid-cols-2 lg:grid-cols-4 gap-4 relative overflow-hidden min-h-[300px] md:min-h-[350px] lg:min-h-[430px]"
+          className="grid grid-cols-2 lg:grid-cols-4 gap-4 relative overflow-hidden min-h-[320px] md:min-h-[350px] lg:min-h-[430px]"
           // style={{ minHeight: 450 }}
         >
         
